69160c3f56e35a88c234e2dc0109e4709882cff3
thamarainagasamy <thamarainagasamy2006@gmail.com>
2025-09-14 11:32:38 +0530

    chnages

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
-
-// Your screens
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
 import Home from './src/screens/Home';
 import Settings from './src/screens/Settings';
 
+const Stack = createStackNavigator();
+
 function App() {
   return (
-    // <Login />
-    <Signup />
-    // <Home />
-
-    // <Settings />
-    
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
